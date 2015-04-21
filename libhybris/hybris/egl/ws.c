fbe376c3c1266f39795c6d18a5d95d4036f0a655
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2015-04-21 13:23:18 +0100

    [dropping] Revert "Revert bionic hook changes and eglSwapBuffers causing problems for SailfishOS"
    This reverts commit 7c49c004778f0606767fdb6150fd12f5c1621a4a.

--- libhybris/hybris/egl/ws.c
+++ libhybris/hybris/egl/ws.c
@@ -91,4 +91,18 @@ const char *ws_eglQueryString(EGLDisplay dpy, EGLint name, const char *(*real_eg
 	return ws->eglQueryString(dpy, name, real_eglQueryString);
 }
 
+void ws_prepareSwap(EGLDisplay dpy, EGLNativeWindowType win, EGLint *damage_rects, EGLint damage_n_rects)
+{
+	_init_ws();
+	if (ws->prepareSwap)
+		ws->prepareSwap(dpy, win, damage_rects, damage_n_rects);
+}
+
+void ws_finishSwap(EGLDisplay dpy, EGLNativeWindowType win)
+{
+	_init_ws();
+	if (ws->finishSwap)
+		ws->finishSwap(dpy, win);
+}
+
 // vim:ts=4:sw=4:noexpandtab
